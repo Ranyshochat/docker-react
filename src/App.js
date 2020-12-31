dc0485b04d4f5e78c547016606088bc309e4a01d
ranyshochat <tomerb@radcom.com>
2020-12-31 11:22:38 +0200

    alter src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          To start again #2 Edit <code>src/App.js</code> and save to reload.
+          To start again #3 Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
